f64d322ff26091ab30fef0658980d3b586bd0c45
ReallyBigTree <TreeAnon@protonmail.com>
2022-05-03 02:45:06 -0400

    BEFORE (unsegregated interface)
    Commit code BEFORE segregating interfaces.

---
+++ App/src/IRaceController.java
@@ -0,0 +1,13 @@
+
+public interface IRaceController {
+
+    public float getSpeed();
+    public int getRank();
+    public int getElapsedTime();
+    public String getRacerName();
+    public String getRacerGear();
+    public int getRacerRankPrediction();
+    public void refreshView();
+
+
+}
---
+++ App/src/Racer.java
@@ -0,0 +1,46 @@
+
+public class Racer implements IRaceController {
+
+	@Override
+	public float getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRank() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getElapsedTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getRacerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRacerGear() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getRacerRankPrediction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void refreshView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
